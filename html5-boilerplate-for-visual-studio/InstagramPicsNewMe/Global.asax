3d6d3385379fb2d99325dc63c91240a284d19225
Burke Holland <burkeholland@gmail.com>
2012-11-07 15:14:05 -0600

    yo dawg. i heard you like instagram.

---
+++ html5-boilerplate-for-visual-studio/InstagramPicsNewMe/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="InstagramPicsNewMe.MvcApplication" Language="C#" %>
---
+++ html5-boilerplate-for-visual-studio/InstagramPicsNewMe/js/script.js
@@ -0,0 +1,24 @@
+﻿window.app = function ($) {
+    var pub = {};
+   
+    pub.init = function () {
+        navigator.geolocation.getCurrentPosition(foundYou);
+    }
+
+    function foundYou(position) {
+        $("#location").html("Looks Like You're At - Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+
+        $.get("https://api.instagram.com/v1/media/search?client_id=4e0171f9fcfc4015bb6300ed91fbf719&count=200",
+              { lat: position.coords.latitude, lng: position.coords.longitude },
+              function (response) {
+                  var html = "";
+                  $(response.data).each(function (index, value) {
+                      html += "<a href='" + this.link + "' target='_blank'><img class='thumbnail' src='" + this.images.thumbnail.url + "' /></a>"
+                  });
+                  $("#pics").html(html);
+              }, 'jsonp');
+    }
+
+    return pub;
+
+}(jQuery);;
